a411e90a24698466425ec4e14739c8fb4af901ce
AdemolaSam <oduntanade2721@gmail.com>
2024-03-24 01:59:12 +0100

    I have created ClassSchedule, Subject, and Note models.

---
+++ server/models/subjects.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const SubjectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    teachers: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Teacher',
+        required: true
+    },
+    classLevels: {
+        type: [String],
+        default: []
+    }
+})
+
+const Subject = mongoose.model('Subject',SubjectSchema);
+module.exports = Subject;
