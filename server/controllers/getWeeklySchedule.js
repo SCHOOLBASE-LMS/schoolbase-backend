f37df6028ab5c49fe91e461998b9b9d600c4caeb
wahab omodolapo <dolwithneybright1@gmail.com>
2024-04-06 10:50:26 +0100

    fixed update bug

--- server/controllers/getWeeklySchedule.js
+++ server/controllers/getWeeklySchedule.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const classSchedule = require("../models/classSchedule");
 
-async function  getScheduleByCurrentWeek(req,res) {
-    try{
+async function getScheduleByCurrentWeek(req, res) {
+    try {
         // get start date of the week 
-           const start = new Date();
-           start.setDate(start.getDate() - start.getDay());
-     
+        const start = new Date();
+        start.setDate(start.getDate() - start.getDay());
+
         //    get end date of the week in ISO format
-           const end = new Date(start);
-           end.setDate(end.getDate() + 6);
-           console.log(start);
-           console.log(end);
+        const end = new Date(start);
+        end.setDate(end.getDate() + 6);
+        // console.log(start);
+        // console.log(end);
 
         //    find from the database 
-        const scheduleForTheWeek = classSchedule.find({
-            date: {$gte: start, $lte:end}
+        const scheduleForTheWeek = await classSchedule.find({
+            date: { $gte: start, $lte: end }
         })
 
         console.log(scheduleForTheWeek)
 
         // check if there is a schedule for the week
-        if(scheduleForTheWeek.length === 0){
-            res.status(404).json({message: "No schedule available for this current week"});
+        if (scheduleForTheWeek.length === 0) {
+            res.status(404).json({ message: "No schedule available for this current week" });
 
         } else {
-            res.status(200).send({message:"Schedule for the current week ", })
+            res.status(200).send({ message: "Schedule for the current week ", data: scheduleForTheWeek })
         }
-    }catch(err){
+    } catch (err) {
         console.log(err.message)
     }
 }
