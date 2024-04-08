b2aaddd29191622e95ddcfd89d1a9d747cb429db
Patrick Oguamanam <kachioguamanam@gmail.com>
2024-04-08 19:52:41 +0100

    unfinished code

--- server/config/env.js
+++ server/config/env.js
@@ -1,11 +1,17 @@
 const dev = {
   SERVER_PORT: process.env.SERVER_DEV_PORT,
-  MONGODB_URI: process.env.MONGODB_DEV_URI
+  MONGODB_URI: process.env.MONGODB_DEV_URI,
+  RP_APIKEY_PUBLIC: process.env.RP_APIKEY_PUBLIC,
+  RP_APIKEY_PRIVATE: process.env.RP_APIKEY_PRIVATE,
+  JWT_SECRET: process.env.JWT_DEV_SECRET
 }
 
 const staging = {
   SERVER_PORT: process.env.SERVER_STAGING_PORT,
-  MONGODB_URI: process.env.MONGODB_STAGING_URI
+  MONGODB_URI: process.env.MONGODB_STAGING_URI,
+  RP_APIKEY_PUBLIC: process.env.RP_APIKEY_PUBLIC,
+  RP_APIKEY_PRIVATE: process.env.RP_APIKEY_PRIVATE,
+  JWT_SECRET: process.env.JWT_STAGING_SECRET
 }
 module.exports = function () {
   switch (process.env.NODE_ENV) {
